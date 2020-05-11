c59831bc42dde5289b03ad24e7a50cc54b6d1376
Eduardo Aparicio Cardenes <EduardoAC@users.noreply.github.com>
2020-05-11 13:53:59 +0100

    Setup gatsby with wordpress templates

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,37 @@
  */
 
 // You can delete this file if you're not using it
+const path = require(`path`)
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    {
+      allWordpressPost(sort: { fields: [date] }) {
+        edges {
+          node {
+            title
+            excerpt
+            slug
+            date(formatString: "MM-DD-YYYY")
+          }
+        }
+      }
+    }
+  `).then(result => {
+    console.log(result)
+    result.data.allWordpressPost.edges.forEach(({ node }) => {
+      createPage({
+        // Decide URL structure
+        path: node.slug,
+        // path to template
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          // This is the $slug variable
+          // passed to blog-post.js
+          slug: node.slug,
+        },
+      })
+    })
+  })
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
@@ -15,7 +15,30 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <h4>Posts</h4>
+    {data.allWordpressPost.edges.map(({ node }) => (
+      <div>
+        <Link to={node.slug}>
+          <p>{node.title}</p>
+        </Link>
+        <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+      </div>
+    ))}
   </Layout>
 )
+
+export const pageQuery = graphql`
+  query {
+    allWordpressPost(sort: { fields: [date] }) {
+      edges {
+        node {
+          title
+          excerpt
+          slug
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
